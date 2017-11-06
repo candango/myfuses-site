7ff348696d66838389323420e3755760dc8a62cc
Flavio Garcia <piraz@candango.org>
2017-11-05 23:46:54 -0500

    Used correct popper version.

--- circuits/main/dspFooter.php
+++ circuits/main/dspFooter.php
@@ -11,8 +11,8 @@
 <!-- Placed at the end of the document so the pages load faster -->
 <script src="/node_modules/jquery/dist/jquery.min.js"></script>
 
-<script src="/node_modules/popper.js/dist/popper.min.js"></script>
-<script src="/node_modules/popper.js/dist/popper-utils.min.js"></script>
+<script src="/node_modules/popper.js/dist/umd/popper.min.js"></script>
+<script src="/node_modules/popper.js/dist/umd/popper-utils.min.js"></script>
 <script src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 </html>
